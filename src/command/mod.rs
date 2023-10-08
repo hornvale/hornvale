25702b4daed32a5958fe728667c89c80f5d310ef
Nathan Douglas <github@darkdell.net>
2023-10-08 16:48:48 -0400

    command_type

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -5,14 +5,14 @@ use crate::game_state::OutputQueueTrait;
 
 pub mod error;
 pub use error::Error as CommandError;
-pub mod r#type;
-pub use r#type::Type as CommandType;
+pub mod command_type;
+pub use command_type::Type as CommandType;
 
 /// The `Command` struct.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Command {
   /// The `Command` type.
-  pub r#type: CommandType,
+  pub command_type: CommandType,
   /// The `Command`'s UUID.
   pub uuid: Uuid,
   /// A backtrace.
@@ -20,19 +20,19 @@ pub struct Command {
 }
 
 impl Command {
-  pub fn new(r#type: CommandType) -> Self {
+  pub fn new(command_type: CommandType) -> Self {
     let uuid = Uuid::new_v4();
-    let backtrace = vec![format!("Command {:?}:{}", r#type, uuid)];
+    let backtrace = vec![format!("Command {:?}:{}", command_type, uuid)];
     Self {
-      r#type,
+      command_type,
       uuid,
       backtrace,
     }
   }
 
   pub fn execute(&self, game_state: &mut GameState) -> Result<(), CommandError> {
-    debug!("Executing {:#?} command.", self.r#type);
-    if let Some(error) = self.r#type.execute(self, game_state).err() {
+    debug!("Executing {:#?} command.", self.command_type);
+    if let Some(error) = self.command_type.execute(self, game_state).err() {
       error!("Error executing command:\n{:#?}\n{:#?}", self.clone(), error);
       game_state.enqueue_output(&error.to_string());
     }
