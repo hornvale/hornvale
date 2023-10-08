7483b9c55dc7098ffb06a30ce7d5bb2f03aced26
Nathan Douglas <github@darkdell.net>
2023-10-08 16:39:32 -0400

    chunk_typ

--- src/chunk/type/mod.rs
+++
@@ -1,7 +0,0 @@
-/// The `ChunkType` enum.
-#[derive(Clone, Debug, Default, Deserialize, Eq, Hash, PartialEq, Serialize)]
-pub enum Type {
-  /// The default, empty `Chunk`.
-  #[default]
-  Default,
-}
