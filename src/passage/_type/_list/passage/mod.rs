70d369959e45cf4f88785b8c397372f46a7c352f
Nathan Douglas <github@darkdell.net>
2023-10-11 22:38:41 -0400

    Comments.

--- src/passage/_type/_list/passage/mod.rs
+++ src/passage/_type/_list/passage/mod.rs
@@ -14,13 +14,13 @@ pub struct Passage {
   /// The UUID of the passage.
   #[builder(default = "Uuid::new_v4().to_string()")]
   pub uuid: String,
-  /// The `Passage`'s status.
+  /// The `Passage`'s status (to be implemented)
   pub status: PassageStatus,
-  /// The `Passage`'s direction.
+  /// The `Passage`'s direction (e.g. North, South, East, West)
   pub direction: PassageDirection,
-  /// The `Passage`'s kind.
+  /// The `Passage`'s kind (e.g. Path, Door, WideOpen)
   pub kind: PassageKind,
-  /// The `Passage`'s target.
+  /// The `Passage`'s target (e.g. Room, Passage, Message)
   pub target: PassageTarget,
   /// The `Passage`'s name.
   pub name: String,
