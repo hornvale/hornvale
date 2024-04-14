bed08de7b019d37c71c18de8ab9aba28763335ed
Nathan Douglas <github@darkdell.net>
2024-04-14 18:02:49 -0400

    Adds some support for multi-word expressions.

--- internal/dictionary/src/part_of_speech/dictionary.rs
+++ internal/dictionary/src/part_of_speech/dictionary.rs
@@ -1,18 +1,28 @@
 use super::PartOfSpeech;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{self, BufRead, Result as IoResult};
+use std::path::Path;
 
 /// A dictionary mapping words to their parts of speech.
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct PartOfSpeechDictionary {
   /// The map of words to their parts of speech.
   pub map: HashMap<String, HashSet<PartOfSpeech>>,
+  /// A list of multi-word expressions.
+  pub multi_word_expressions: Vec<String>,
 }
 
 impl PartOfSpeechDictionary {
   /// Creates a new dictionary.
   pub fn new() -> Self {
-    Self { map: HashMap::new() }
+    let map = HashMap::new();
+    let multi_word_expressions = Vec::new();
+    Self {
+      map,
+      multi_word_expressions,
+    }
   }
 
   /// Adds a word to the dictionary.
@@ -103,14 +113,102 @@ impl PartOfSpeechDictionary {
   pub fn is_determiner(&self, word: &str) -> bool {
     self.fits(word, PartOfSpeech::Determiner)
   }
+
+  /// Loads words from a file into the dictionary as a given part of speech.
+  pub fn load_words_from_file(&mut self, path: &Path, pos: PartOfSpeech) -> IoResult<()> {
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+      let word = line?;
+      self.add_word(word, pos);
+    }
+
+    Ok(())
+  }
+}
+
+macro_rules! read_into_dictionary {
+  ($dictionary:ident, $path:expr, $pos:expr) => {
+    let words = include_str!($path);
+    for word in words.lines() {
+      if word.is_empty() {
+        continue;
+      }
+      if word.contains(' ') {
+        $dictionary.multi_word_expressions.push(word.to_string());
+        $dictionary.add_word(word.to_string().replace(' ', "_"), $pos);
+      } else {
+        $dictionary.add_word(word.to_string(), $pos);
+      }
+    }
+  };
+}
+
+fn load_default_dictionary() -> PartOfSpeechDictionary {
+  let mut dictionary = PartOfSpeechDictionary::new();
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/adjectives.txt",
+    PartOfSpeech::Adjective
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/adverbs.txt",
+    PartOfSpeech::Adverb
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/conjunctions.txt",
+    PartOfSpeech::Conjunction
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/determiners.txt",
+    PartOfSpeech::Determiner
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/interjections.txt",
+    PartOfSpeech::Interjection
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/nouns.txt",
+    PartOfSpeech::Noun
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/prepositions.txt",
+    PartOfSpeech::Preposition
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/pronouns.txt",
+    PartOfSpeech::Pronoun
+  );
+  read_into_dictionary!(
+    dictionary,
+    "../../data/part_of_speech/dictionary/verbs.txt",
+    PartOfSpeech::Verb
+  );
+  dictionary
+}
+
+impl Default for PartOfSpeechDictionary {
+  fn default() -> Self {
+    load_default_dictionary()
+  }
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
+  use hornvale_test_utilities::prelude::*;
 
   #[test]
   fn test_add_word() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("cat".to_string(), PartOfSpeech::Noun);
     assert_eq!(dictionary.len(), 1);
@@ -118,6 +216,7 @@ mod tests {
 
   #[test]
   fn test_get_parts_of_speech() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("cat".to_string(), PartOfSpeech::Noun);
     let parts_of_speech = dictionary.get_parts_of_speech("cat");
@@ -126,6 +225,7 @@ mod tests {
 
   #[test]
   fn test_contains_word() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("cat".to_string(), PartOfSpeech::Noun);
     assert!(dictionary.contains_word("cat"));
@@ -134,6 +234,7 @@ mod tests {
 
   #[test]
   fn test_remove_word() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("cat".to_string(), PartOfSpeech::Noun);
     dictionary.remove_word("cat");
@@ -142,6 +243,7 @@ mod tests {
 
   #[test]
   fn test_clear() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("cat".to_string(), PartOfSpeech::Noun);
     dictionary.clear();
@@ -150,6 +252,7 @@ mod tests {
 
   #[test]
   fn test_is_noun() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("cat".to_string(), PartOfSpeech::Noun);
     assert!(dictionary.is_noun("cat"));
@@ -158,6 +261,7 @@ mod tests {
 
   #[test]
   fn test_is_pronoun() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("I".to_string(), PartOfSpeech::Pronoun);
     assert!(dictionary.is_pronoun("I"));
@@ -166,6 +270,7 @@ mod tests {
 
   #[test]
   fn test_is_verb() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("run".to_string(), PartOfSpeech::Verb);
     assert!(dictionary.is_verb("run"));
@@ -174,6 +279,7 @@ mod tests {
 
   #[test]
   fn test_is_adjective() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("big".to_string(), PartOfSpeech::Adjective);
     assert!(dictionary.is_adjective("big"));
@@ -182,6 +288,7 @@ mod tests {
 
   #[test]
   fn test_is_adverb() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("quickly".to_string(), PartOfSpeech::Adverb);
     assert!(dictionary.is_adverb("quickly"));
@@ -190,6 +297,7 @@ mod tests {
 
   #[test]
   fn test_is_preposition() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("in".to_string(), PartOfSpeech::Preposition);
     assert!(dictionary.is_preposition("in"));
@@ -198,6 +306,7 @@ mod tests {
 
   #[test]
   fn test_is_conjunction() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("and".to_string(), PartOfSpeech::Conjunction);
     assert!(dictionary.is_conjunction("and"));
@@ -206,6 +315,7 @@ mod tests {
 
   #[test]
   fn test_is_interjection() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("wow".to_string(), PartOfSpeech::Interjection);
     assert!(dictionary.is_interjection("wow"));
@@ -214,9 +324,17 @@ mod tests {
 
   #[test]
   fn test_is_determiner() {
+    init();
     let mut dictionary = PartOfSpeechDictionary::new();
     dictionary.add_word("the".to_string(), PartOfSpeech::Determiner);
     assert!(dictionary.is_determiner("the"));
     assert!(!dictionary.is_determiner("a"));
+  }
+
+  #[test]
+  fn test_default() {
+    init();
+    let dictionary = PartOfSpeechDictionary::default();
+    assert_gt!(dictionary.len(), 0);
   }
 }
