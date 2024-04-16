91a88c9b9c926e98291ab12dc6c26d2149272a3e
Nathan Douglas <github@darkdell.net>
2024-04-16 12:40:46 -0400

    Expand CommandContext.

--- internal/command/src/command/context.rs
+++ internal/command/src/command/context.rs
@@ -2,10 +2,33 @@ use hecs::Entity;
 use serde::{Deserialize, Serialize};
 
 /// The command context.
-#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct CommandContext {
+  /// The original command string.
+  ///
+  /// This is the string that the player typed in to invoke the command. We
+  /// provide it here in case the command needs to be re-parsed or re-processed
+  /// in some way, or if the command needs to be echoed back to the player.
+  pub command: String,
+  /// The verb of the command.
+  ///
+  /// This is the first word of the command string, which is typically the verb
+  /// that the player is trying to perform. For example, in the command "take
+  /// sword", the verb is "take".
+  ///
+  /// This may not always be the case, however; some commands have synonyms or
+  /// aliases that are not verbs, etc.
+  pub verb: String,
   /// The direct object, if any.
+  ///
+  /// This is the object that the player is trying to interact with. For example,
+  /// in the command "take sword", the direct object is "sword".
   pub direct_object: Option<Entity>,
   /// The indirect object, if any.
+  ///
+  /// This is the object that the player is trying to interact with, but which is
+  /// not the primary object of the command. For example, in the command "give
+  /// sword to guard", the direct object is "sword" and the indirect object is
+  /// "guard".
   pub indirect_object: Option<Entity>,
 }
