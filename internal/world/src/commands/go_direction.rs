67edcb2a98b433f22372828ddb917e0ea5e2b502
Nathan Douglas <github@darkdell.net>
2024-04-18 17:35:18 -0400

    World commands.

--- internal/world/src/commands/go_direction.rs
+++ internal/world/src/commands/go_direction.rs
@@ -1,19 +1,22 @@
 use hecs::World;
-use hornvale_command::prelude::{Command, CommandContext, CommandError};
+use hornvale_command::prelude::*;
 
 /// Attempt to walk in a given direction.
 #[derive(Clone, Copy, Debug)]
 pub struct GoDirectionCommand;
 
 impl Command for GoDirectionCommand {
-  const NAME: &'static str = "go-direction";
-  const SYNONYMS: &'static [&'static str] = &["go", "move", "walk", "enter", "exit"];
+  const NAME: &'static str = "go";
+  const SYNONYMS: &'static [&'static str] = &["go"];
   const BRIEF: &'static str = "Attempt to walk in a given direction.";
   const DESCRIPTION: &'static str = r#"
     Attempt to walk in a given direction. The player will attempt to move in the
     direction specified by the first argument. If the player is unable to move in
     that direction, they will be informed of the reason why.
   "#;
+  const FORM: CommandForm = CommandForm::Direction;
+  const ARITY: CommandArity = CommandArity::Unary(CommandParameter::Direction);
+
   fn execute(_world: &mut World, _context: &CommandContext) -> Result<(), CommandError> {
     Err(CommandError::UnknownError)
   }
--- internal/world/src/commands/look_here.rs
+++ internal/world/src/commands/look_here.rs
@@ -1,18 +1,21 @@
 use hecs::World;
-use hornvale_command::prelude::{Command, CommandContext, CommandError};
+use hornvale_command::prelude::*;
 
 /// Look at here.
 #[derive(Clone, Copy, Debug)]
 pub struct LookHereCommand;
 
 impl Command for LookHereCommand {
-  const NAME: &'static str = "look-here";
+  const NAME: &'static str = "look";
   const SYNONYMS: &'static [&'static str] = &["look"];
   const BRIEF: &'static str = "Look at your surroundings.";
   const DESCRIPTION: &'static str = r#"
     Look at your surroundings. This command will provide a description of the
     player's current location, including any items or characters that are present.
   "#;
+  const FORM: CommandForm = CommandForm::Here;
+  const ARITY: CommandArity = CommandArity::Nullary;
+
   fn execute(_world: &mut World, _context: &CommandContext) -> Result<(), CommandError> {
     Err(CommandError::UnknownError)
   }
