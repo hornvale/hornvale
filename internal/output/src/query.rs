ad7d7e113efbcd19f7e7d7ff71aea9bd9dc4cf82
Nathan Douglas <github@darkdell.net>
2024-04-19 17:14:24 -0400

    Functions.

--- internal/output/src/query.rs
+++ internal/output/src/query.rs
@@ -1,31 +1,26 @@
 use crate::prelude::*;
-use hecs::{Entity, World};
-use std::sync::OnceLock;
-
-/// Get the output entity.
-pub fn get_out_entity(world: &mut World) -> &'static Entity {
-  static ENTITY: OnceLock<Entity> = OnceLock::new();
-  ENTITY.get_or_init(|| world.spawn((StdoutQueue::default(),)))
-}
-
-/// Get the error output entity.
-pub fn get_err_entity(world: &mut World) -> &'static Entity {
-  static ENTITY: OnceLock<Entity> = OnceLock::new();
-  ENTITY.get_or_init(|| world.spawn((StderrQueue::default(),)))
-}
+use hecs::World;
 
 /// Enqueue an output message.
 pub fn enqueue_output(world: &mut World, message: String) {
-  let entity = get_out_entity(world);
-  let queue = world.query_one_mut::<&mut StdoutQueue>(*entity).unwrap();
-  queue.0.enqueue(message);
+  if let Some(queue) = world.query_mut::<&mut StdoutQueue>().into_iter().next() {
+    queue.1 .0.enqueue(message);
+  } else {
+    let mut queue = StdoutQueue::default();
+    queue.0.enqueue(message);
+    world.spawn((queue,));
+  }
 }
 
 /// Enqueue an error message.
 pub fn enqueue_error(world: &mut World, message: String) {
-  let entity = get_err_entity(world);
-  let queue = world.query_one_mut::<&mut StderrQueue>(*entity).unwrap();
-  queue.0.enqueue(message);
+  if let Some(queue) = world.query_mut::<&mut StderrQueue>().into_iter().next() {
+    queue.1 .0.enqueue(message);
+  } else {
+    let mut queue = StderrQueue::default();
+    queue.0.enqueue(message);
+    world.spawn((queue,));
+  }
 }
 
 #[cfg(test)]
@@ -39,10 +34,9 @@ mod tests {
     let mut world = World::new();
     enqueue_output(&mut world, "test".to_string());
     enqueue_output(&mut world, "test2".to_string());
-    let entity = get_out_entity(&mut world);
-    let queue = world.query_one_mut::<&mut StdoutQueue>(*entity).unwrap();
-    assert_eq!(queue.0.dequeue(), Some("test".to_string()));
-    assert_eq!(queue.0.dequeue(), Some("test2".to_string()));
+    let queue = world.query_mut::<&mut StdoutQueue>().into_iter().next().unwrap();
+    assert_eq!(queue.1 .0.dequeue().unwrap(), "test");
+    assert_eq!(queue.1 .0.dequeue().unwrap(), "test2");
   }
 
   #[test]
@@ -51,9 +45,8 @@ mod tests {
     let mut world = World::new();
     enqueue_error(&mut world, "test".to_string());
     enqueue_error(&mut world, "test2".to_string());
-    let entity = get_err_entity(&mut world);
-    let queue = world.query_one_mut::<&mut StderrQueue>(*entity).unwrap();
-    assert_eq!(queue.0.dequeue(), Some("test".to_string()));
-    assert_eq!(queue.0.dequeue(), Some("test2".to_string()));
+    let queue = world.query_mut::<&mut StderrQueue>().into_iter().next().unwrap();
+    assert_eq!(queue.1 .0.dequeue().unwrap(), "test");
+    assert_eq!(queue.1 .0.dequeue().unwrap(), "test2");
   }
 }
