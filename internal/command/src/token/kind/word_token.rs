0ce6b1f74f3c9b86e669492fb1a1f1b542d13549
Nathan Douglas <github@darkdell.net>
2024-04-24 08:10:19 -0400

    Removes stray workspace dependency.

---
+++ internal/command/src/token/kind/word_token.rs
@@ -0,0 +1,17 @@
+use serde::{Deserialize, Serialize};
+
+/// Different kinds of adjectives or nouns.
+#[derive(Clone, Copy, Debug, Default, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub enum WordToken {
+  /// Unclassified.
+  #[default]
+  Unclassified,
+  /// Verb.
+  Verb,
+  /// Noun.
+  Noun,
+  /// Adjective.
+  Adjective,
+  /// Ambiguous.
+  Ambiguous,
+}
