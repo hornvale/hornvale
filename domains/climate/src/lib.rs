0cca39c4a76eab15dd631a25d5128357d14667cc
Nathan Douglas <github@darkdell.net>
2026-07-06 19:59:45 -0400

    feat(climate): habitability classification + habitable fraction

--- domains/climate/src/lib.rs
+++ domains/climate/src/lib.rs
@@ -3,11 +3,13 @@
 
 pub mod biome;
 pub mod circulation;
+pub mod habitability;
 pub mod moisture;
 pub mod temperature;
 
 pub use biome::{Biome, SeafloorFeature};
 pub use circulation::{RotationRegime, band_count_for};
+pub use habitability::{habitable_fraction, is_habitable};
 
 use hornvale_kernel::{
     ConceptRegistry, ObserverContext, PhenomenaSource, Phenomenon, Position, RegistryError,
